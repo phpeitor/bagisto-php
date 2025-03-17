5bb1021a0b64217149a9e421aab142bcad776279
FORTEL\alejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-03-16 19:29:09 -0500

    theme commit

--- packages/Webkul/Installer/src/Database/Seeders/Shop/ThemeCustomizationTableSeeder.php
+++ packages/Webkul/Installer/src/Database/Seeders/Shop/ThemeCustomizationTableSeeder.php
@@ -206,7 +206,7 @@ class ThemeCustomizationTableSeeder extends Seeder
                                 'limit' => 12,
                             ],
                         ]),
-                    ], [
+                    ], /*[
                         'theme_customization_id' => 5,
                         'locale'                 => $locale,
                         'options'                => json_encode([
@@ -222,7 +222,7 @@ class ThemeCustomizationTableSeeder extends Seeder
 
                             'css'  => '.section-gap{margin-top:80px}.direction-ltr{direction:ltr}.direction-rtl{direction:rtl}.inline-col-wrapper{display:grid;grid-template-columns:auto 1fr;grid-gap:60px;align-items:center}.inline-col-wrapper .inline-col-image-wrapper{overflow:hidden}.inline-col-wrapper .inline-col-image-wrapper img{max-width:100%;height:auto;border-radius:16px;text-indent:-9999px}.inline-col-wrapper .inline-col-content-wrapper{display:flex;flex-wrap:wrap;gap:20px;max-width:464px}.inline-col-wrapper .inline-col-content-wrapper .inline-col-title{max-width:442px;font-size:60px;font-weight:400;color:#060c3b;line-height:70px;font-family:DM Serif Display;margin:0}.inline-col-wrapper .inline-col-content-wrapper .inline-col-description{margin:0;font-size:18px;color:#6e6e6e;font-family:Poppins}@media (max-width:991px){.inline-col-wrapper{grid-template-columns:1fr;grid-gap:16px}.inline-col-wrapper .inline-col-content-wrapper{gap:10px}} @media (max-width:768px){.inline-col-wrapper .inline-col-image-wrapper img {width:100%;} .inline-col-wrapper .inline-col-content-wrapper .inline-col-title{font-size:28px !important;line-height:normal !important}} @media (max-width:525px){.inline-col-wrapper .inline-col-content-wrapper .inline-col-title{font-size:20px !important;} .inline-col-description{font-size:16px} .inline-col-wrapper{grid-gap:10px}}',
                         ]),
-                    ], [
+                    ],*/ [
                         'theme_customization_id' => 7,
                         'locale'                 => $locale,
                         'options'                => json_encode([
